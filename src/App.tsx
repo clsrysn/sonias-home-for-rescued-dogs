f119c8f5cc7994527b7bcaea85976ee050818226
KuraKat <ae10082017@gmail.com>
2026-03-04 06:19:42 +0800

    Removed unnecessary dependencies and files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Gallery from "./pages/Gallery";
@@ -11,26 +10,22 @@ import DogProfile from "./pages/DogProfile";
 import SocialMedia from "./pages/SocialMedia";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/dog/:id" element={<DogProfile />} />
-          <Route path="/donate" element={<Donate />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/updates" element={<SocialMedia />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/dog/:id" element={<DogProfile />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/updates" element={<SocialMedia />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  </TooltipProvider>
 );
 
 export default App;
